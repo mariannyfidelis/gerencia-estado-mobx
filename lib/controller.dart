e99c12a4b0bb52bb4cc5ccb29e8df0f3659f92b4
Marianny Fidelis <mariannyfidelis@gmail.com>
2022-01-17 20:58:27 -0300

    Implementação inicial de Gerência de estado com o Mobx

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,5 +1,15 @@
 import 'package:mobx/mobx.dart';
 
 class Controller {
+  //O mobx trabalha com uma Tríade (Observable - Action - Reaction) que causam efeitos
   var counter = Observable(0);
+  late Action increment;
+
+  Controller() {
+    increment = Action(_increment);
+  }
+
+  _increment() {
+    counter.value += 1;
+  }
 }
